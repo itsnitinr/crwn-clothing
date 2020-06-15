bbced4d597b790257158a92ee4a9c303f261178a
Nitin Ranganath <nitinranganath@gmail.com>
2020-06-15 16:05:33 +0530

    Implement routing using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/HomePage.component";
 
 const App = () => {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 };
