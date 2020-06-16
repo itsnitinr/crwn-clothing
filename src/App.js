8a9abc790279e82a00c25d275ae26c6e0a36c6f0
Nitin Ranganath <nitinranganath@gmail.com>
2020-06-16 15:28:07 +0530

    Build sign in component with custom styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/HomePage.component";
 import ShopPage from "./pages/shop/ShopPage.component";
+import RegisterLoginPage from "./pages/register-and-login/RegisterLoginPage.component";
 import Header from "./components/header/Header.component";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={RegisterLoginPage} />
       </Switch>
     </div>
   );
