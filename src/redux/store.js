de5a79c6b76fb565513b72c6d5445b18ade4f4fe
Nitin Ranganath <nitinranganath@gmail.com>
2020-06-24 19:42:42 +0530

    Log redux actions and state only in development mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,12 @@ import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(
   rootReducer,
